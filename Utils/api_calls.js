8e386904df5493b47b1430262c9cd61d620acbc8
Pourbaix Michaël <mi.pourbaix@students.ephec.be>
2023-05-30 16:27:20 +0200

    [ADD] feat(api_calls): Adding some API test calls

--- Utils/api_calls.js
+++ Utils/api_calls.js
@@ -1,1 +1,67 @@
 // We will be testing here some API calls to the Steam API
+const axios = require("axios");
+const axiosCookiejarSupport = require("axios-cookiejar-support");
+const toughCookie = require("tough-cookie");
+
+require("dotenv").config();
+
+const main_route = "https://api.steampowered.com/";
+
+let payload = {
+	key: process.env.STEAM_API_KEY,
+	steamid: "hultryme",
+	appid: "730",
+};
+
+class SteamAPI {
+	constructor(key) {
+		this.api_key = key;
+	}
+
+	async getPlayerAchievements(userid, appid) {
+		let response = await axios.get(
+			main_route + "ISteamUserStats/GetPlayerAchievements/v1/",
+			{
+				params: {
+					key: this.api_key,
+					steamid: userid,
+					appid: appid,
+				},
+			}
+		);
+		return response;
+	}
+
+	async getUserStatsForGame(userid, appid) {
+		let response = await axios.get(
+			main_route + "ISteamUserStats/GetUserStatsForGame/v2/",
+			{
+				params: {
+					key: this.api_key,
+					steamid: userid,
+					appid: appid,
+				},
+			}
+		);
+		return response;
+	}
+
+	// async getCsStats(userid) {
+	// 	let response = await axios.get(
+	// 		"https://public-api.tracker.gg/v2/csgo/standard/profile/steam/76561198074295219",
+	// 		{
+	// 			params: {
+	// 				key: this.api_key,
+	// 			},
+	// 		}
+	// 	);
+	// }
+}
+
+async function testClass() {
+	let test = new SteamAPI(process.env.STEAM_API_KEY);
+	let res = await test.getUserStatsForGame("76561198074295219");
+	console.log(res);
+}
+
+testClass();
